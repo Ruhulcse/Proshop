177c841be72c6f97105d37db295f86d29eba73a9
Ruhulcse <ruhul.cse7862@gmail.com>
2020-11-09 13:45:01 +0600

    Dummy product added in homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import React from 'react'
 import {Container} from 'react-bootstrap'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
+import HomeScreen from './screens/HomeScreen'
 const App = () => {
   return (
     <>
     <Header/>
     <main className='py-3'>
       <Container>
-        <h1>Welcome To ProShop</h1>
+        <HomeScreen/>
       </Container>
     </main>
     <Footer/>
