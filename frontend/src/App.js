601cb8d52df858ddf38dba009c70e16811e526d2
Ruhulcse <ruhul.cse7862@gmail.com>
2020-11-09 15:44:38 +0600

    react router septup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,22 @@
 import React from 'react'
+import { BrowserRouter as Router,Route } from 'react-router-dom'
 import {Container} from 'react-bootstrap'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 const App = () => {
   return (
-    <>
-    <Header/>
-    <main className='py-3'>
-      <Container>
-        <HomeScreen/>
-      </Container>
-    </main>
-    <Footer/>
-    </>
+    <Router>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomeScreen} exact/>
+          <Route path='/product/:id' component={ProductScreen}/>
+        </Container>
+      </main>
+      <Footer/>
+    </Router>
   );
 }
 
