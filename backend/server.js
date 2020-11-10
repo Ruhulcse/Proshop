2a921209bd53d0b53dccfdd88fbf6ec0856fef43
Ruhulcse <ruhul.cse7862@gmail.com>
2020-11-10 14:24:32 +0600

    ES feature added for backend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
+
 
 dotenv.config()
 
 const app = express();
 
 app.get('/',(req,res) => {
-    res.send('API is running....')
+    res.send('API is running...')
 })
 
 app.get('/api/products',(req,res)=>{
