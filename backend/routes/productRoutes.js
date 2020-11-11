383d51feb523340a58327a6bb5adebad5c291e29
Ruhulcse <ruhul.cse7862@gmail.com>
2020-11-11 12:42:32 +0600

    Error middleware added

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -7,7 +7,6 @@ import Product from '../models/productModel.js'
 //@route   GET /api/products
 //@access  Public
 router.get('/',asyncHandler(async(req,res)=>{
-    console.log("requested")
     const products = await Product.find({})
     res.json(products)
 }))
@@ -21,7 +20,8 @@ router.get('/:id',asyncHandler(async(req,res)=>{
     if(product){
         res.json(product)
     }else{
-        res.status(400).json({message: 'Product not found'})
+        res.status(404)
+        throw new Error('Product not found')
     }
 
 }))
