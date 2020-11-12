3976383622fb2cadce01bcaa1cf3db9895dad556
Ruhulcse <ruhul.cse7862@gmail.com>
2020-11-12 12:35:03 +0600

    Add to cart functionality

--- frontend/src/Components/Loader.js
+++ frontend/src/Components/Loader.js
@@ -12,7 +12,7 @@ function Loader() {
             display: 'block'
         }}
         >
-            <span class='sr-only'>Loading....</span>
+            <span className='sr-only'>Loading....</span>
         </Spinner>
     )
 }
