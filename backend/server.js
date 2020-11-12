0fcc4606e0c5804b90ee21b6bcc7f418a05f3823
Ruhulcse <ruhul.cse7862@gmail.com>
2020-11-12 16:57:02 +0600

    User Authentication Endpoint

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
 
+import userRoutes from './routes/userRouters.js'
+
 
 dotenv.config()
 
@@ -13,12 +15,14 @@ connectDB()
 
 const app = express();
 
+app.use(express.json())
+
 app.get('/',(req,res) => {
     res.send('API is running...')
 })
 
 app.use('/api/products',productRoutes)
-
+app.use('/api/users', userRoutes)
 app.use(notFound)
 app.use(errorHandler)
 const PORT = process.env.PORT ||5000
