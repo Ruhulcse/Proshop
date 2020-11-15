60da97a5503d2bc39fefd796a1672d331a4716a3
Ruhulcse <ruhul.cse7862@gmail.com>
2020-11-15 12:03:14 +0600

    Custom Authentication middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
-
+import generateToken from '../utils/generateToken.js'
 //@desc    Auth user & get token
 //@route   POST /api/users/login
 //@access  Public
@@ -13,7 +13,7 @@ const authUser = asyncHandler(async (req,res) => {
            name: user.name,
            email: user.email,
            isAdmin: user.isAdmin,
-           token: null,
+           token: generateToken(user._id),
        })
    }else{
        res.status(401)
@@ -21,4 +21,23 @@ const authUser = asyncHandler(async (req,res) => {
    }
 })
 
-export { authUser }
+//@desc    Get user profile
+//@route   GET/api/users/profile
+//@access  Private
+const getUserProfile = asyncHandler(async (req,res) => {
+     const user = await User.findById(req.user._id)
+
+     if(user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+     } else{
+         res.status(404)
+         throw new Error('User not found')
+     }
+ })
+
+export { authUser ,getUserProfile}
