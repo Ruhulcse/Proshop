fd46f859879fa80026db8293ed543fcde7142cf5
Ruhulcse <ruhul.cse7862@gmail.com>
2020-11-18 13:23:34 +0600

    Order screen

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -45,6 +45,11 @@ const orderSchema = mongoose.Schema({
         required: true,
         default: 0.0,
     },
+    totalPrice: {
+        type: Number,
+        required: true,
+        default: 0.0,
+      },
     isPaid: {
         type: Boolean,
         required: true,
