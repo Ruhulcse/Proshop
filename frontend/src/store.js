ca181307205d01ad85fb53a7db7f651b15373861
Ruhulcse <ruhul.cse7862@gmail.com>
2020-11-19 13:06:26 +0600

    show orders on profile

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,15 +5,21 @@ import {
     productListReducer,
     productDetailsReducer
 } from './reducers/productReducers'
+
 import {cartReducer} from './reducers/cartReducers'
+
 import {
     userLoginReducer,
     userRegisterReducer,
     userDetailsReducer,
     userUpdateProfileReducer
 } from './reducers/userReducers'
+
 import {
-  orderCreateReducer, orderDetailsReducer, orderPayReducer
+  orderCreateReducer, 
+  orderDetailsReducer, 
+  orderListMyReducer,
+   orderPayReducer
 } from './reducers/orderReducers'
 
 const reducer = combineReducers({
@@ -26,7 +32,8 @@ const reducer = combineReducers({
     userUpdateProfile: userUpdateProfileReducer,
     orderCreate: orderCreateReducer,
     orderDetails: orderDetailsReducer,
-    orderPay: orderPayReducer
+    orderPay: orderPayReducer,
+    orderListMy: orderListMyReducer
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
