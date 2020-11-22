89cb1711e569cf1903eed35a591369bebc7adffe
Ruhulcse <ruhul.cse7862@gmail.com>
2020-11-22 16:38:04 +0600

    update user functionality

--- frontend/src/screens/UserEditScreen.js
+++ frontend/src/screens/UserEditScreen.js
@@ -5,7 +5,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import Message from '../Components/Message'
 import Loader from '../Components/Loader'
 import FormContainer from '../Components/FormContainer'
-import { getUserDetails } from '../actions/userActions'
+import { getUserDetails ,updateUser} from '../actions/userActions'
+import { USER_UPDATE_RESET } from '../constants/userConstants'
 
 const UserEditScreen = ({match,history}) => {
     const userId = match.params.id
@@ -19,19 +20,33 @@ const UserEditScreen = ({match,history}) => {
     const userDetails = useSelector(state=> state.userDetails)
     const {loading, error, user } = userDetails
 
+    const userUpdate = useSelector(state=> state.userUpdate)
+    const {
+        loading: loadingUpdate,
+        error: errorUpdate,
+        success:successUpdate 
+    } = userUpdate
+
 
     useEffect(()=>{
-        if(!user.name || user._id !== userId){
-            dispatch(getUserDetails(userId))
+        if(successUpdate){
+            dispatch({ type: USER_UPDATE_RESET})
+            history.push('/admin/userlist')
         }else{
-            setName(user.name)
-            setEmail(user.email)
-            setIsAdmin(user.isAdmin)
+            if(!user.name || user._id !== userId){
+                dispatch(getUserDetails(userId))
+            }else{
+                setName(user.name)
+                setEmail(user.email)
+                setIsAdmin(user.isAdmin)
+            }
         }
-    },[user,dispatch,userId])
+    },[user,dispatch,userId,successUpdate,history])
 
     const submitHandler = (e) => {
+        console.log("clicked")
         e.preventDefault()
+        dispatch(updateUser({_id: userId, name, email, isAdmin}))
         
     }
     return (
@@ -39,6 +54,8 @@ const UserEditScreen = ({match,history}) => {
           <Link to='/admin/userList' className='btn btn-light my-3'>Go back</Link>
            <FormContainer>
             <h1>Edit User</h1>
+            {loadingUpdate && <Loader/>}
+            {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
             {loading ? <Loader/> : error ? <Message variant='danger'>{error}</Message>: (
                 <Form onSubmit={submitHandler}>
                 <Form.Group controlId='name'>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -14,7 +14,8 @@ import {
     userDetailsReducer,
     userUpdateProfileReducer,
     userListReducer,
-    userDeleteReducer
+    userDeleteReducer,
+    userUpdateReducer
 } from './reducers/userReducers'
 
 import {
@@ -34,6 +35,7 @@ const reducer = combineReducers({
     userUpdateProfile: userUpdateProfileReducer,
     userList : userListReducer,
     userDelete: userDeleteReducer,
+    userUpdate : userUpdateReducer,
     orderCreate: orderCreateReducer,
     orderDetails: orderDetailsReducer,
     orderPay: orderPayReducer,
