030d44bef42b963541ada44a1eaa1e1a835fe47b
Ruhulcse <ruhul.cse7862@gmail.com>
2020-11-23 15:57:26 +0600

    update deliverd status in backend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -73,6 +73,26 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
     }
 })
 
+// @desc    Update order to delivered
+// @route   GET /api/orders/:id/deliver
+// @access  Private/Admin
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id)
+  
+    if (order) {
+      order.isDelivered = true
+      order.deliveredAt = Date.now()
+  
+      const updatedOrder = await order.save()
+  
+      res.json(updatedOrder)
+    } else {
+      res.status(404)
+      throw new Error('Order not found')
+    }
+  })
+
+
 //@desc   get logged in user orders
 //@route  GET /api/orders/myorders
 //@access  private
@@ -89,4 +109,11 @@ const getOrders = asyncHandler(async (req, res) => {
   res.json(orders)
 })
 
-export {addOrderItems, getOrderById,updateOrderToPaid,getMyOrders,getOrders}
+export {
+    addOrderItems, 
+    getOrderById,
+    updateOrderToPaid,
+    getMyOrders,
+    getOrders,
+    updateOrderToDelivered
+}
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express'
 const router = express.Router();
 import { 
-    addOrderItems, getMyOrders, getOrderById, getOrders, updateOrderToPaid
+    addOrderItems, getMyOrders, getOrderById, getOrders, updateOrderToDelivered, updateOrderToPaid
 } from '../controllers/orderController.js'
 import { protect ,admin} from '../middleware/authMiddleWare.js'
 
@@ -9,5 +9,5 @@ router.route('/').post(protect,addOrderItems).get(protect, admin, getOrders)
 router.route('/myorders').get(protect,getMyOrders)
 router.route('/:id').get(protect,getOrderById)
 router.route('/:id/pay').put(protect,updateOrderToPaid)
-
+router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered)
 export default router
