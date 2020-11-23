e7770f42ede093ce8ce60f22c19875a42e886e88
Ruhulcse <ruhul.cse7862@gmail.com>
2020-11-23 14:08:49 +0600

    Get all order backend

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express'
 const router = express.Router();
 import { 
-    addOrderItems, getMyOrders, getOrderById, updateOrderToPaid
+    addOrderItems, getMyOrders, getOrderById, getOrders, updateOrderToPaid
 } from '../controllers/orderController.js'
-import { protect } from '../middleware/authMiddleWare.js'
+import { protect ,admin} from '../middleware/authMiddleWare.js'
 
-router.route('/').post(protect,addOrderItems)
+router.route('/').post(protect,addOrderItems).get(protect, admin, getOrders)
 router.route('/myorders').get(protect,getMyOrders)
 router.route('/:id').get(protect,getOrderById)
 router.route('/:id/pay').put(protect,updateOrderToPaid)
