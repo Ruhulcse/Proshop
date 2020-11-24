27b3b85e0beb20f765b80b3cd8377f169b2057c1
Ruhulcse <ruhul.cse7862@gmail.com>
2020-11-24 11:46:37 +0600

    Front end product review

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,7 +6,8 @@ import {
     productDetailsReducer,
     productDeleteReducer,
     productCreateReducer,
-    productUpdateReducer
+    productUpdateReducer,
+    productReviewCreateReducer
 } from './reducers/productReducers'
 
 import {cartReducer} from './reducers/cartReducers'
@@ -36,6 +37,7 @@ const reducer = combineReducers({
     productDelete: productDeleteReducer,
     productCreate: productCreateReducer,
     productUpdate: productUpdateReducer,
+    productReviewCreate: productReviewCreateReducer,
     cart : cartReducer,
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
